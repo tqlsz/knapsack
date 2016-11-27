04ea54b08d7e302731ba1a8dee005dbd44a819fb
tangqinglong <tangqinglong@tangqinglongdeMac-mini.local>
2016-11-27 18:23:23 +0800

    repair tree

--- tree.py
+++ tree.py
@@ -47,7 +47,7 @@ class Tree():
         self.recur_preorder_trvalsal(root.l_child)
         self.recur_preorder_trvalsal(root.r_child)
 
-    def stack_preorder_tralsal(self, root):
+    def stack_preorder_trvalsal(self, root):
         '''堆栈实现先序遍历'''
         stack = []
         temp_node = root
@@ -74,6 +74,36 @@ class Tree():
         print root.get()
         self.recur_midorder_trvalsal(root.r_child)
 
+    def stack_midorder_trvalsal(self, root):
+        '''堆栈实现中序遍历'''
+        temp_node = root
+        my_stack = []
+        while temp_node or my_stack:
+            if temp_node is not None:
+                my_stack.append(temp_node)
+                temp_node = temp_node.l_child
+            else:
+                temp_node = my_stack.pop()
+                print temp_node.get()
+                temp_node = temp_node.r_child
+        '''
+        while temp_node:
+            if temp_node.l_child:
+                my_stack.append(temp_node)
+                temp_node = temp_node.l_child
+            else:
+                print temp_node.get()
+                if my_stack == []:
+                    return
+                temp_node = my_stack.pop()
+                print temp_node.get()
+                while temp_node.r_child is None:
+                    if my_stack == []:
+                        return
+                    temp_node = my_stack.pop()
+                    print temp_node.get()
+                temp_node = temp_node.r_child
+        '''
     def recur_postorder_trvalsal(self, root):
         '''递归实现后序遍历'''
         if root is None:
@@ -82,9 +112,25 @@ class Tree():
         self.recur_postorder_trvalsal(root.r_child)
         print root.get()
 
+    def stack_postorder_trvalsal(self, root):
+        '''堆栈实现后序遍历'''
+        my_stack1 = []
+        my_stack2 = []
+        temp_node = root
+        while temp_node or my_stack1:
+            if temp_node:
+                my_stack2.append(temp_node)
+                my_stack1.append(temp_node)
+                temp_node = temp_node.r_child
+            else:
+                temp_node = my_stack1.pop()
+                temp_node = temp_node.l_child
+        while my_stack2:
+            print my_stack2.pop().get()
+
 
 if __name__ == '__main__':
     tree = Tree()
     for i in range(10):
         tree.add_node(Node(i))
-    tree.stack_preorder_tralsal(tree.root)
+    tree.stack_postorder_trvalsal(tree.root)
